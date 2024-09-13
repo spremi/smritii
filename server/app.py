ba432274bb6c44a087f1695d3b061f8c87e85f47
Sanjeev Premi <spremi@ymail.com>
2024-09-14 02:33:30 +0530

    feat: s: app: Extract EXIF data from images
    Signed-off-by: Sanjeev Premi <spremi@ymail.com>

--- server/app.py
+++ server/app.py
@@ -11,7 +11,9 @@ from flask import Flask, abort, jsonify, render_template, request, send_file
 from http import HTTPStatus
 from pathlib import Path
 
+import json
 import os
+import piexif
 
 from . import constants as const
 
@@ -43,6 +45,11 @@ Size of thumbnail
 THUMB_EXTN = '.jpg'
 """
 Extension for thumbnails.
+"""
+
+DIR_INFO = 'info'
+"""
+Base (sub)directory for info.
 """
 
 
@@ -132,6 +139,233 @@ def create_app(config=None):
             return send_file('static/images/video-thumb.png')
 
         return send_file('static/images/404.png')
+
+    @app.route('/info/<path:location>', methods=['GET'])
+    def get_info(location):
+        """
+        Returns information related to the image.
+        It includes (if present) caption, exif data, copyright, etc.
+        For consistency, location refers to 'actual' image - not the file containing.
+
+        The information is cached on first access.
+        (Can be refreshed via query param)
+        """
+        global BASE_ALBUM, BASE_META, DIR_INFO
+
+        arg_location = Path(location)
+
+        image_path = os.path.abspath(os.path.join(BASE_ALBUM, location))
+
+        #
+        # Weed out incorrect urls early.
+        #
+        if not image_path.startswith(BASE_ALBUM) or not os.path.isfile(image_path):
+            abort(HTTPStatus.BAD_REQUEST)
+
+        #
+        # Should meta information be refreshed?
+        #
+        refresh = 0 if request.args.get('refresh') in (None, '', '0') else 1
+
+        arg_name = arg_location.name
+        arg_stem = arg_location.stem
+        arg_path = arg_location.parent
+
+        info_name = const.INFO_PREFIX + arg_stem + const.INFO_EXTN
+
+        info_path = os.path.abspath(os.path.join(BASE_META, DIR_INFO, arg_path, info_name))
+
+        if not info_path.startswith(BASE_META):
+            return jsonify({'status': 'ERROR', 'exif': None})
+
+        if os.path.isfile(info_path) and refresh == 0:
+            return send_file(info_path)
+
+        stat = os.stat(image_path)
+
+        info_dir = os.path.dirname(info_path)
+        os.makedirs(info_dir, exist_ok=True)
+
+        info = {
+            'caption': '',
+            'data': {}
+        }
+
+        exif_data = {
+            const.T_FILE_NAME: arg_name,
+            const.T_FILE_SIZE: stat.st_size
+        }
+
+        im = Image.open(image_path)
+
+        im_exif = im.info.get('exif')
+
+        if im_exif:
+            exif_dict = piexif.load(im_exif)
+
+            if '0th' in exif_dict:
+                for tag in exif_dict['0th']:
+                    # print(f'= {tag} =')
+                    tag_name = piexif.TAGS['0th'][tag]["name"]
+
+                    if tag_name == 'Make':
+                        exif_data[const.T_CAMERA_MAKE] = exif_dict['0th'][tag].decode('utf-8')
+
+                    if tag_name == 'Model':
+                        exif_data[const.T_CAMERA_MODEL] = exif_dict['0th'][tag].decode('utf-8')
+
+                    if tag_name == 'Orientation':
+                        v = exif_dict['0th'][tag]
+
+                        exif_data[const.T_ORIENTATION] = const.ExifOrientation[v]
+
+                    if tag_name == 'ResolutionUnit':
+                        v = exif_dict['0th'][tag]
+
+                        exif_data[const.T_RESOLUTION_UNIT] = const.ExifResolutionUnit.get(v, f'({v})')
+
+                    if tag_name == 'XResolution':
+                        v = exif_dict['0th'][tag]
+                        r = v[0] / v[1]
+
+                        exif_data[const.T_X_RESOLUTION] = f'{r:.0f}'
+
+                    if tag_name == 'YResolution':
+                        v = exif_dict['0th'][tag]
+                        r = v[0] / v[1]
+
+                        exif_data[const.T_Y_RESOLUTION] = f'{r:.0f}'
+
+                    if tag_name == 'ImageDescription':
+                        exif_data[const.T_DESCRIPTION] = exif_dict['0th'][tag].decode('utf-8').strip()
+
+                if 'Exif' in exif_dict:
+                    for tag in exif_dict['Exif']:
+                        # print(f'= {tag} =')
+                        tag_name = piexif.TAGS['Exif'][tag]["name"]
+
+                        if tag_name == 'ExifVersion':
+                            exif_data[const.T_EXIF_VERSION] = exif_dict['Exif'][tag].decode('utf-8')
+
+                        if tag_name == 'PixelXDimension':
+                            exif_data[const.T_IMAGE_WIDTH] = exif_dict['Exif'][tag]
+
+                        if tag_name == 'PixelYDimension':
+                            exif_data[const.T_IMAGE_HEIGHT] = exif_dict['Exif'][tag]
+
+                        if tag_name == 'DateTimeOriginal':
+                            exif_data[const.T_TS_ORIGINAL] = exif_dict['Exif'][tag].decode('utf-8')
+
+                        if tag_name == 'FocalLength':
+                            v = exif_dict['Exif'][tag]
+                            r = v[0] / v[1]
+
+                            exif_data[const.T_FOCAL_LENGTH] = f'{r:.1f} mm'
+
+                        if tag_name == 'ISOSpeedRatings':
+                            exif_data[const.T_ISO_SPEED] = exif_dict['Exif'][tag]
+
+                        if tag_name == 'Flash':
+                            v = exif_dict['Exif'][tag]
+
+                            exif_data[const.T_FLASH] = const.ExifFlash.get(v, f'({v})')
+
+                        if tag_name == 'ExposureMode':
+                            v = exif_dict['Exif'][tag]
+
+                            exif_data[const.T_EXPOSURE_MODE] = const.ExifExposureMode.get(v, f'({v})')
+
+                        if tag_name == 'ExposureTime':
+                            v = exif_dict['Exif'][tag]
+                            r = v[0] / v[1]
+
+                            exif_data[const.T_EXPOSURE_TIME] = f'{r:.8f}'
+
+                        if tag_name == 'ExposureBiasValue':
+                            v = exif_dict['Exif'][tag]
+                            r = v[0] / v[1]
+
+                            exif_data[const.T_EXPOSURE_BIAS] = f'{r:.1f} EV'
+
+                        if tag_name == 'FNumber':
+                            v = exif_dict['Exif'][tag]
+                            r = v[0] / v[1]
+
+                            exif_data[const.T_F_NUMBER] = f'{r:2.1f}'
+
+                        if tag_name == 'MeteringMode':
+                            v = exif_dict['Exif'][tag]
+
+                            exif_data[const.T_METERING_MODE] = const.ExifMeteringMode.get(v, f'({v})')
+
+                        if tag_name == 'LightSource':
+                            v = exif_dict['Exif'][tag]
+
+                            exif_data[const.T_LIGHT_SOURCE] = const.ExifLightSource[v]
+
+                        if tag_name == 'WhiteBalance':
+                            v = exif_dict['Exif'][tag]
+
+                            exif_data[const.T_WHITE_BALANCE] = const.ExifWhiteBalance[v]
+
+                        if tag_name == 'DigitalZoomRatio':
+                            v = exif_dict['Exif'][tag]
+                            r = v[0] / v[1]
+
+                            exif_data[const.T_DIGITAL_ZOOM] = f'{r:2.1f}'
+
+                        if tag_name == 'UserComment':
+                            exif_data[const.T_USER_COMMENT] = exif_dict['Exif'][tag].decode('utf-8')
+
+                if 'GPS' in exif_dict:
+                    for tag in exif_dict['GPS']:
+                        tag_name = piexif.TAGS['GPS'][tag]["name"]
+
+                        if tag_name == 'GPSLatitudeRef':
+                            exif_data[const.T_GPS_LATITUDE_REF] = exif_dict['GPS'][tag].decode('utf-8')
+
+                        if tag_name == 'GPSLatitude':
+                            v = exif_dict['GPS'][tag]
+
+                            lat_deg = v[0][0] / v[0][1]
+                            lat_min = v[1][0] / v[1][1]
+                            lat_sec = v[2][0] / v[2][1]
+
+                            exif_data[const.T_GPS_LATITUDE] = f'{lat_deg:.2f}/{lat_min:.2f}/{lat_sec:.3f}'
+
+                        if tag_name == 'GPSLongitudeRef':
+                            exif_data[const.T_GPS_LONGITUDE_REF] = exif_dict['GPS'][tag].decode('utf-8')
+
+                        if tag_name == 'GPSLongitude':
+                            v = exif_dict['GPS'][tag]
+
+                            lon_deg = v[0][0] / v[0][1]
+                            lon_min = v[1][0] / v[1][1]
+                            lon_sec = v[2][0] / v[2][1]
+
+                            exif_data[const.T_GPS_LONGITUDE] = f'{lon_deg:.2f}/{lon_min:.2f}/{lon_sec:.3f}'
+
+                        if tag_name == 'GPSDateStamp':
+                            exif_data[const.T_GPS_TS_DATE] = exif_dict['GPS'][tag].decode('utf-8').strip()
+
+                        if tag_name == 'GPSTimeStamp':
+                            v = exif_dict['GPS'][tag]
+
+                            ts_hh = v[0][0] / v[0][1]
+                            ts_mm = v[1][0] / v[1][1]
+                            ts_ss = v[2][0] / v[2][1]
+
+                            exif_data[const.T_GPS_TS_TIME] = f'{ts_hh:2.0f}:{ts_mm:2.0f}:{ts_ss:4.2f}'
+
+                        if tag_name == 'GPSMapDatum':
+                            exif_data[const.T_GPS_MAP_DATUM] = exif_dict['GPS'][tag].decode('utf-8').strip()
+
+        info['data'] = exif_data
+
+        with open(info_path, 'w') as f:
+            json.dump(info, f, indent=2)
+
+        return jsonify(info)
 
     #
     # Return application object.
--- server/constants.py
+++ server/constants.py
@@ -25,3 +25,158 @@ EXTN_SUPPORTED = EXTN_IMAGE + EXTN_AUDIO + EXTN_VIDEO
 """
 All supported extensions
 """
+
+INFO_PREFIX = 'inf_'
+"""
+Prefix for information file
+"""
+
+INFO_EXTN = '.json'
+"""
+Suffix for information file
+"""
+
+#
+# Tag names for meta information stored for images.
+#
+T_EXIF_VERSION = 1
+T_FILE_NAME = 2
+T_FILE_SIZE = 3
+T_IMAGE_WIDTH = 4
+T_IMAGE_HEIGHT = 5
+T_CAMERA_MAKE = 6
+T_CAMERA_MODEL = 7
+T_ORIENTATION = 8
+T_RESOLUTION_UNIT = 9
+T_X_RESOLUTION = 10
+T_Y_RESOLUTION = 11
+T_TS_ORIGINAL = 12
+T_FOCAL_LENGTH = 13
+T_APERTURE = 14
+T_ISO_SPEED = 15
+T_FLASH = 16
+T_FLASH_BIAS = 17
+T_EXPOSURE_MODE = 18
+T_EXPOSURE_TIME = 19
+T_EXPOSURE_BIAS = 20
+T_F_NUMBER = 21
+T_SHUTTER_SPEED = 22
+T_METERING_MODE = 23
+T_LIGHT_SOURCE = 24
+T_WHITE_BALANCE = 25
+T_IMAGE_QUALITY = 26
+T_DIGITAL_ZOOM = 27
+T_DESCRIPTION = 28
+T_USER_COMMENT = 29
+T_COPYRIGHT = 30
+T_GPS_LATITUDE_REF = 31
+T_GPS_LATITUDE = 32
+T_GPS_LONGITUDE_REF = 33
+T_GPS_LONGITUDE = 34
+T_GPS_TS_DATE = 35
+T_GPS_TS_TIME = 36
+T_GPS_MAP_DATUM = 37
+
+#
+# Resolution units defined in EXIF standard.
+#
+ExifResolutionUnit = {
+    1: 'Not defined',
+    2: 'Inch',
+    3: 'Centimeter',
+}
+
+#
+# Different orientations sources defined in EXIF standard.
+#
+ExifOrientation = {
+    1: 'Normal',
+    2: 'Mirror Horizontal',
+    3: 'Upside-down (rotated 180deg)',
+    4: 'Mirror Vertical',
+    5: 'Mirror Horizontal & Rotated 90deg counter-clockwise',
+    6: 'Rotated 90deg counter-clockwise',
+    7: 'Mirror Horizontal & Rotated 90deg clockwise',
+    8: 'Rotated 90deg clockwise',
+}
+
+#
+# Different light sources defined in EXIF standard.
+#
+ExifLightSource = {
+    0: 'Unknown',
+    1: 'Daylight',
+    2: 'Fluorescent',
+    3: 'Tungsten',
+    4: 'Flash',
+    9: 'Fine',
+    10: 'Cloudy',
+    11: 'Shade',
+    12: 'Daylight Fluorescent',
+    13: 'Day White Fluorescent',
+    14: 'Cool White Fluorescent',
+    15: 'White Fluorescent',
+    17: 'Standard Light A',
+    18: 'Standard Light B',
+    19: 'Standard Light C',
+    20: 'D55',
+    21: 'D65',
+    22: 'D75',
+    23: 'D50',
+    24: 'ISO',
+    255: 'Other',
+}
+
+#
+# Different metering modes defined in EXIF standard.
+#
+ExifMeteringMode = {
+    0: 'Unknown',
+    1: 'Average',
+    2: 'Center Weighted Average',
+    3: 'Spot',
+    4: 'Multi Spot',
+    5: 'Pattern',
+    6: 'Partial',
+    255: 'Other',
+}
+
+#
+# Exposure modes defined in EXIF standard.
+#
+ExifExposureMode = {
+    0: 'Auto',
+    1: 'Manual',
+    2: 'Auto Bracket',
+}
+
+#
+# Different flash modes defined in EXIF standard.
+#
+ExifFlash = {
+    0: 'Not used',
+    1: 'Used',
+}
+
+#
+# Different white balance modes defined in EXIF standard.
+#
+ExifWhiteBalance = {
+    0: 'AUTO',
+    1: 'MANUAL',
+}
+
+#
+# Exposure programs defined in EXIF standard.
+#
+ExifExposureProgram = {
+    0: 'Not defined',
+    1: 'Manual',
+    2: 'Normal Program',
+    3: 'Aperture Priority',
+    4: 'Shutter Priority',
+    5: 'Creative Program',
+    6: 'Action Program',
+    7: 'Portrait Mode',
+    8: 'Landscape Mode',
+}
