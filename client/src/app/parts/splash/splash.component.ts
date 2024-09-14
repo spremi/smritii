4016be36a8c6b079d7f68c509391bd21fb7be639
Sanjeev Premi <spremi@ymail.com>
2024-09-14 16:53:02 +0530

    chore: c: app: Add component 'splash'
    Command:
      ng g component parts/splash --style=sass
    
    Signed-off-by: Sanjeev Premi <spremi@ymail.com>

---
+++ client/src/app/parts/splash/splash.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'sp-splash',
+  standalone: true,
+  imports: [],
+  templateUrl: './splash.component.html',
+  styleUrl: './splash.component.sass'
+})
+export class SplashComponent {
+
+}
