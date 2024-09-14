df01c0d40417e31f5fa5851aeff27a8f951d9c1d
Sanjeev Premi <spremi@ymail.com>
2024-09-14 23:06:56 +0530

    feat: c: models: Define Image
    Signed-off-by: Sanjeev Premi <spremi@ymail.com>

--- client/src/app/models/image.ts
+++ client/src/app/models/image.ts
@@ -1,2 +1,34 @@
+//
+// [smritii]
+//
+// Sanjeev Premi <spremi@ymail.com>
+//
+// BSD-3-Clause License
+//
+
+
+import { ImageInfo, initImageInfo } from '@models/image-info';
+
+/**
+ * Encapsulates information of an image.
+ */
 export interface Image {
+  id: string;
+  seq: number;
+  name: string;
+  info?: ImageInfo;
 }
+
+/**
+ * Initializer for 'Image'.
+ */
+export const initImage = (
+  argId?: string,
+  argSeq?: number,
+  argName?: string,
+  argInfo?: ImageInfo): Image => ({
+    id: argId ? argId : '',
+    seq: argSeq ? argSeq : -1,
+    name: argName ? argName : '',
+    info: argInfo ? argInfo : initImageInfo()
+  });
