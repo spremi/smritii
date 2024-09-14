b5f7c0fc0265a11ec242881b7e7cbd2e3eab9fae
Sanjeev Premi <spremi@ymail.com>
2024-09-14 16:25:08 +0530

    chore: c: app: Add route 'view'
    Command:
      ng g component routes/view --style=sass
    
    Signed-off-by: Sanjeev Premi <spremi@ymail.com>

---
+++ client/src/app/routes/view/view.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'sp-view',
+  standalone: true,
+  imports: [],
+  templateUrl: './view.component.html',
+  styleUrl: './view.component.sass'
+})
+export class ViewComponent {
+
+}
