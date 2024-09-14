754f559762bd90b0dec9d323310f368c0ef904c3
Sanjeev Premi <spremi@ymail.com>
2024-09-14 22:59:57 +0530

    feat: c: models: Define Album
    Signed-off-by: Sanjeev Premi <spremi@ymail.com>

--- client/src/app/models/album.ts
+++ client/src/app/models/album.ts
@@ -1,2 +1,40 @@
+//
+// [smritii]
+//
+// Sanjeev Premi <spremi@ymail.com>
+//
+// BSD-3-Clause License
+//
+
+
+/**
+ * Represents a folder containing albums and/or images.
+ */
 export interface Album {
+  /**
+   * Unique ID of the album.
+   */
+  id: string;
+
+  /**
+   * Sequence in which album is shown.
+   */
+  seq: number;
+
+  /**
+   * Name of the album.
+   */
+  name: string;
 }
+
+/**
+ * Initializer for 'Album'.
+ */
+export const initAlbum = (
+  argId?: string,
+  argSeq?: number,
+  argName?: string): Album => ({
+    id: argId ? argId : '',
+    seq: argSeq ? argSeq : -1,
+    name: argName ? argName : '',
+  });
