76147a273d0f3a815fd7e1df94aa49b5bfc36221
Sanjeev Premi <spremi@ymail.com>
2024-09-14 16:25:04 +0530

    chore: c: app: Add route 'e404'
    Command:
      ng g component routes/e404 --style=sass
    
    Signed-off-by: Sanjeev Premi <spremi@ymail.com>

---
+++ client/src/app/routes/e404/e404.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'sp-e404',
+  standalone: true,
+  imports: [],
+  templateUrl: './e404.component.html',
+  styleUrl: './e404.component.sass'
+})
+export class E404Component {
+
+}
