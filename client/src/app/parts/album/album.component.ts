364d4364e04a48a9cb34bf5b0356927e9a216734
Sanjeev Premi <spremi@ymail.com>
2024-09-15 02:09:46 +0530

    feat: c: parts: album: Initial implementation
    Signed-off-by: Sanjeev Premi <spremi@ymail.com>

--- client/src/app/parts/album/album.component.ts
+++ client/src/app/parts/album/album.component.ts
@@ -1,12 +1,35 @@
-import { Component } from '@angular/core';
+//
+// [smritii]
+//
+// Sanjeev Premi <spremi@ymail.com>
+//
+// BSD-3-Clause License
+//
+
+import { Component, HostListener, Input } from '@angular/core';
+
+import { Album } from '@models/album';
+import { BasenamePipe } from '@pipes/basename.pipe';
+import { StateService } from '@services/state.service';
 
 @Component({
   selector: 'sp-album',
   standalone: true,
-  imports: [],
+  imports: [
+    BasenamePipe
+  ],
   templateUrl: './album.component.html',
   styleUrl: './album.component.sass'
 })
 export class AlbumComponent {
+  @Input() data: Album | undefined;
 
+  constructor(private stateSvc: StateService) {}
+
+  @HostListener('click')
+  open(): void {
+    if (this.data) {
+      this.stateSvc.setLocation(this.data.name);
+    }
+  }
 }
