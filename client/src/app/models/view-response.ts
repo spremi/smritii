4359b71dada014075ab301d3c96a027d197df31b
Sanjeev Premi <spremi@ymail.com>
2024-09-14 23:13:59 +0530

    feat: c: models: Define ViewResponse
    Signed-off-by: Sanjeev Premi <spremi@ymail.com>

--- client/src/app/models/view-response.ts
+++ client/src/app/models/view-response.ts
@@ -1,2 +1,25 @@
+//
+// [smritii]
+//
+// Sanjeev Premi <spremi@ymail.com>
+//
+// BSD-3-Clause License
+//
+
+import { Album } from '@models/album';
+import { Image } from '@models/image';
+
+/**
+ * Encapsulates response from server to ViewRequest.
+ */
 export interface ViewResponse {
+  /**
+   * List of albums at specified location.
+   */
+  albums: Album[],
+
+  /**
+   * List of images at specified location.
+   */
+  images: Image[],
 }
