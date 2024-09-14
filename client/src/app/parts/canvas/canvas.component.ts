544022aea9e64826ee80f37145639c01eed4b1ed
Sanjeev Premi <spremi@ymail.com>
2024-09-14 16:52:16 +0530

    chore: c: app: Add component 'canvas'
    Command:
      ng g component parts/canvas --style=sass
    
    Signed-off-by: Sanjeev Premi <spremi@ymail.com>

---
+++ client/src/app/parts/canvas/canvas.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'sp-canvas',
+  standalone: true,
+  imports: [],
+  templateUrl: './canvas.component.html',
+  styleUrl: './canvas.component.sass'
+})
+export class CanvasComponent {
+
+}
