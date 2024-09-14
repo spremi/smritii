07f12900b916178897ecc738312132091d73a3ac
Sanjeev Premi <spremi@ymail.com>
2024-09-14 23:29:58 +0530

    chore: c: config: Include Animations
    Signed-off-by: Sanjeev Premi <spremi@ymail.com>

--- client/src/app/app.config.ts
+++ client/src/app/app.config.ts
@@ -11,11 +11,13 @@ import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { provideHttpClient } from '@angular/common/http';
+import { provideAnimations } from '@angular/platform-browser/animations';
 
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
+    provideAnimations(),
     provideHttpClient(),
     provideRouter(routes),
     provideZoneChangeDetection({ eventCoalescing: true }),
