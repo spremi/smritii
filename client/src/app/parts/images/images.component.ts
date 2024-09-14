05a202bc15d93fb381104c90fd5eb81652b275b1
Sanjeev Premi <spremi@ymail.com>
2024-09-15 03:44:28 +0530

    feat: c: parts: images: Initial implementation
    Signed-off-by: Sanjeev Premi <spremi@ymail.com>

--- client/src/app/parts/images/images.component.ts
+++ client/src/app/parts/images/images.component.ts
@@ -1,12 +1,27 @@
-import { Component } from '@angular/core';
+//
+// [smritii]
+//
+// Sanjeev Premi <spremi@ymail.com>
+//
+// BSD-3-Clause License
+//
+
+
+import { Component, Input } from '@angular/core';
+
+import { Image } from '@models/image';
+import { ImageComponent } from '@parts/image/image.component';
 
 @Component({
   selector: 'sp-images',
   standalone: true,
-  imports: [],
+  imports: [
+    ImageComponent,
+  ],
   templateUrl: './images.component.html',
   styleUrl: './images.component.sass'
 })
 export class ImagesComponent {
+  @Input() images: Image[] = [];
 
 }
