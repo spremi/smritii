074693f5031371716146fadce7a366fbb4bf20a6
Sanjeev Premi <spremi@ymail.com>
2024-09-15 04:42:25 +0530

    feat: c: parts: image-info: Initial implementation
    Signed-off-by: Sanjeev Premi <spremi@ymail.com>

--- client/src/app/parts/image-info/image-info.component.ts
+++ client/src/app/parts/image-info/image-info.component.ts
@@ -1,4 +1,15 @@
-import { Component } from '@angular/core';
+//
+// [smritii]
+//
+// Sanjeev Premi <spremi@ymail.com>
+//
+// BSD-3-Clause License
+//
+
+
+import { Component, Input, OnInit } from '@angular/core';
+
+import { ImageInfo, InfoData, InfoLabel } from '@models/image-info';
 
 @Component({
   selector: 'sp-image-info',
@@ -7,6 +18,18 @@ import { Component } from '@angular/core';
   templateUrl: './image-info.component.html',
   styleUrl: './image-info.component.sass'
 })
-export class ImageInfoComponent {
+export class ImageInfoComponent implements OnInit{
+  @Input() info: ImageInfo | undefined;
 
+  keys: string[] = [];
+  data: InfoData = {};
+
+  readonly labels = InfoLabel;
+
+  ngOnInit(): void {
+    if (this.info) {
+      this.data = this.info.data;
+      this.keys = Object.keys(this.data).sort((a, b) => parseInt(a) - parseInt(b));
+    }
+  }
 }
