320d450ae2f5198f246a1990903313a7b2680a9d
Sanjeev Premi <spremi@ymail.com>
2024-09-14 16:24:59 +0530

    chore: c: app: Add route 'about'
    Command:
      ng g component routes/about --style=sass
    
    Signed-off-by: Sanjeev Premi <spremi@ymail.com>

---
+++ client/src/app/routes/about/about.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'sp-about',
+  standalone: true,
+  imports: [],
+  templateUrl: './about.component.html',
+  styleUrl: './about.component.sass'
+})
+export class AboutComponent {
+
+}
