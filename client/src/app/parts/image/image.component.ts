e63f86d440d7b95c5392d15ae230c539f32076cc
Sanjeev Premi <spremi@ymail.com>
2024-09-15 03:44:05 +0530

    feat: c: parts: image: Initial implementation
    Signed-off-by: Sanjeev Premi <spremi@ymail.com>

--- client/src/app/parts/image/image.component.ts
+++ client/src/app/parts/image/image.component.ts
@@ -1,12 +1,43 @@
-import { Component } from '@angular/core';
+//
+// [smritii]
+//
+// Sanjeev Premi <spremi@ymail.com>
+//
+// BSD-3-Clause License
+//
+
+
+import { Component, HostListener, Input } from '@angular/core';
+import { Router } from '@angular/router';
+
+import { Image } from '@models/image';
+import { BasenamePipe } from '@pipes/basename.pipe';
+import { StateService } from '@services/state.service';
 
 @Component({
   selector: 'sp-image',
   standalone: true,
-  imports: [],
+  imports: [
+    BasenamePipe,
+  ],
   templateUrl: './image.component.html',
   styleUrl: './image.component.sass'
 })
 export class ImageComponent {
+  @Input() data: Image | undefined;
 
+  constructor(private router: Router,
+    private stateSvc: StateService
+  ) {}
+
+  @HostListener('click')
+  open(): void {
+    if (this.data) {
+      // Select image for canvas
+      this.stateSvc.setSelected(this.data);
+
+      // Transition to image show
+      this.router.navigate(['view']);
+    }
+  }
 }
