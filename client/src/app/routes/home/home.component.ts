914d2dd4a6c31150f4212d63d38d951d6cbe0754
Sanjeev Premi <spremi@ymail.com>
2024-09-14 16:24:42 +0530

    chore: c: app: Add route 'home'
    Command:
      ng g component routes/home --style=sass
    
    Signed-off-by: Sanjeev Premi <spremi@ymail.com>

---
+++ client/src/app/routes/home/home.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'sp-home',
+  standalone: true,
+  imports: [],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.sass'
+})
+export class HomeComponent {
+
+}
