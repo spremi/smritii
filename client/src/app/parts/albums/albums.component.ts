bf679a5bf76e859447d8e4beeec10f53f9356494
Sanjeev Premi <spremi@ymail.com>
2024-09-15 02:16:38 +0530

    chore: c: parts: albums: Initial implementation
    Signed-off-by: Sanjeev Premi <spremi@ymail.com>

--- client/src/app/parts/albums/albums.component.ts
+++ client/src/app/parts/albums/albums.component.ts
@@ -1,12 +1,28 @@
-import { Component } from '@angular/core';
+//
+// [smritii]
+//
+// Sanjeev Premi <spremi@ymail.com>
+//
+// BSD-3-Clause License
+//
+
+
+import { Component, Input } from '@angular/core';
+
+import { Album } from '@models/album';
+import { AlbumComponent } from '@parts/album/album.component';
 
 @Component({
   selector: 'sp-albums',
   standalone: true,
-  imports: [],
+  imports: [
+    AlbumComponent
+  ],
   templateUrl: './albums.component.html',
   styleUrl: './albums.component.sass'
 })
 export class AlbumsComponent {
+  @Input() albums: Album[] = [];
 
+  constructor() {}
 }
