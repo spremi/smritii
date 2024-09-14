927f9ad6acfc048ba54ca048e4c3e8a10b489bff
Sanjeev Premi <spremi@ymail.com>
2024-09-14 23:27:20 +0530

    chore: c: config: Include HttpClient
    Signed-off-by: Sanjeev Premi <spremi@ymail.com>

--- client/src/app/app.config.ts
+++ client/src/app/app.config.ts
@@ -1,8 +1,23 @@
+//
+// [smritii]
+//
+// Sanjeev Premi <spremi@ymail.com>
+//
+// BSD-3-Clause License
+//
+
+
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
+
+import { provideHttpClient } from '@angular/common/http';
 
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [
+    provideHttpClient(),
+    provideRouter(routes),
+    provideZoneChangeDetection({ eventCoalescing: true }),
+  ]
 };
