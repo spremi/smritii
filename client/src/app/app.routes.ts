7eb8fd26af465e52fbfdf4f27c92b385505f5da1
Sanjeev Premi <spremi@ymail.com>
2024-09-14 17:13:58 +0530

    feat: c: app: routes: Initial wiring
    Signed-off-by: Sanjeev Premi <spremi@ymail.com>

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,3 +1,37 @@
-import { Routes } from '@angular/router';
+//
+// [smritii]
+//
+// Sanjeev Premi <spremi@ymail.com>
+//
+// BSD-3-Clause License
+//
 
-export const routes: Routes = [];
+import { Routes } from '@angular/router';
+import { AboutComponent } from '@routes/about/about.component';
+import { E404Component } from '@routes/e404/e404.component';
+import { HomeComponent } from '@routes/home/home.component'
+import { ViewComponent } from '@routes/view/view.component';
+
+export const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: '/home'
+  },
+  {
+    path: 'about',
+    component: AboutComponent
+  },
+  {
+    path: 'home',
+    component: HomeComponent
+  },
+  {
+    path: 'view',
+    component: ViewComponent
+  },
+  {
+    path: '**',
+    component: E404Component
+  },
+];
