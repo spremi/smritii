1e89c63f517ddd9b5e520481cb0faf207eefe083
Sanjeev Premi <spremi@ymail.com>
2024-09-14 16:44:05 +0530

    chore: c: app: Add component 'albums'
    Command:
      ng g component parts/albums --style=sass
    
    Signed-off-by: Sanjeev Premi <spremi@ymail.com>

---
+++ client/src/app/parts/albums/albums.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'sp-albums',
+  standalone: true,
+  imports: [],
+  templateUrl: './albums.component.html',
+  styleUrl: './albums.component.sass'
+})
+export class AlbumsComponent {
+
+}
