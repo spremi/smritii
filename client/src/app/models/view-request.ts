687b04a3a7717ab0f21a375e8c9d698e3928cc6b
Sanjeev Premi <spremi@ymail.com>
2024-09-14 23:13:31 +0530

    feat: c: models: Define ViewRequest
    Signed-off-by: Sanjeev Premi <spremi@ymail.com>

--- client/src/app/models/view-request.ts
+++ client/src/app/models/view-request.ts
@@ -1,2 +1,42 @@
+//
+// [smritii]
+//
+// Sanjeev Premi <spremi@ymail.com>
+//
+// BSD-3-Clause License
+//
+
+
+/**
+ * Encapsulates request to server to fetch contents of an album.
+ */
 export interface ViewRequest {
+  /**
+   * Path to album from base.
+   */
+  location: string;
+
+  /**
+   * Force rescan onf the location, instead of getting a cached response.
+   */
+  refresh?: boolean;
 }
+
+
+/**
+ * Initializer for 'ViewRequest'.
+ */
+export const initViewRequest = (
+  argPath: string,
+  argRefresh?: boolean
+): ViewRequest => {
+  const ret: ViewRequest = {
+    location: argPath
+  }
+
+  if (argRefresh) {
+    ret.refresh = argRefresh;
+  }
+
+  return ret;
+};
