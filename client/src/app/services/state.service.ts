1a1227cb3dd1c72b5456b8c7badcb419ccaef729
Sanjeev Premi <spremi@ymail.com>
2024-09-15 00:50:16 +0530

    feat: c: svc: Implement StateService
    For now, save current location.
    
    Signed-off-by: Sanjeev Premi <spremi@ymail.com>

--- client/src/app/services/state.service.ts
+++ client/src/app/services/state.service.ts
@@ -1,9 +1,32 @@
+//
+// [smritii]
+//
+// Sanjeev Premi <spremi@ymail.com>
+//
+// BSD-3-Clause License
+//
+
+
 import { Injectable } from '@angular/core';
+import { BehaviorSubject, distinctUntilChanged, filter, Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class StateService {
 
+  private location$: BehaviorSubject<string> = new BehaviorSubject<string>('');
+
   constructor() { }
+
+  setLocation(location: string): void {
+    this.location$.next(location);
+  }
+
+  getLocation(): Observable<string> {
+    return this.location$.asObservable().pipe(
+      filter(c => c !== null),
+      distinctUntilChanged()
+    );
+  }
 }
