cbf1306dccd15ebc266dd2741998d285b8375f92
Sanjeev Premi <spremi@ymail.com>
2024-09-14 23:22:39 +0530

    feat: c: models: Define Pebble
    Signed-off-by: Sanjeev Premi <spremi@ymail.com>

--- client/src/app/models/pebble.ts
+++ client/src/app/models/pebble.ts
@@ -1,2 +1,27 @@
+//
+// [smritii]
+//
+// Sanjeev Premi <spremi@ymail.com>
+//
+// BSD-3-Clause License
+//
+
+
+/**
+ * Encapsulates link to a specific album.
+ * List of pebbles describe path to current album.
+ */
 export interface Pebble {
+  tag: string;
+  link: string;
 }
+
+/**
+ * Initializer for 'Pebble'.
+ */
+export const initPebble = (
+  argTag?: string,
+  argLink?: string): Pebble => ({
+    tag: argTag ? argTag : '',
+    link: argLink ? argLink : '',
+  });
