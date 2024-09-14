af4e666c842d27e9f3cda15b394ffd445806bc62
Sanjeev Premi <spremi@ymail.com>
2024-09-14 16:42:53 +0530

    chore: c: app: Add component 'images'
    Command:
      ng g component parts/images --style=sass
    
    Signed-off-by: Sanjeev Premi <spremi@ymail.com>

---
+++ client/src/app/parts/images/images.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'sp-images',
+  standalone: true,
+  imports: [],
+  templateUrl: './images.component.html',
+  styleUrl: './images.component.sass'
+})
+export class ImagesComponent {
+
+}
