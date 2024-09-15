6b987df67477c1d556829d5d976976f3e2d7e1cc
Sanjeev Premi <spremi@ymail.com>
2024-09-15 22:51:06 +0530

    feat: c: parts: image: Show thumbnail
    Signed-off-by: Sanjeev Premi <spremi@ymail.com>

--- client/src/app/parts/image/image.component.ts
+++ client/src/app/parts/image/image.component.ts
@@ -7,11 +7,12 @@
 //
 
 
-import { Component, HostListener, Input } from '@angular/core';
+import { Component, HostListener, Input, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 
 import { Image } from '@models/image';
 import { BasenamePipe } from '@pipes/basename.pipe';
+import { DataService } from '@services/data.service';
 import { StateService } from '@services/state.service';
 
 @Component({
@@ -23,12 +24,23 @@ import { StateService } from '@services/state.service';
   templateUrl: './image.component.html',
   styleUrl: './image.component.sass'
 })
-export class ImageComponent {
+export class ImageComponent implements OnInit {
   @Input() data: Image | undefined;
 
+  readonly THUMB_PATH = '/thumb';
+
+  thumbUrl = '';
+
   constructor(private router: Router,
+    private dataSvc: DataService,
     private stateSvc: StateService
   ) {}
+
+  ngOnInit(): void {
+    if (this.data) {
+      this.thumbUrl = this.dataSvc.getBaseUrl() + this.THUMB_PATH + this.data.name;
+    }
+  }
 
   @HostListener('click')
   open(): void {
