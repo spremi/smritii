d3de96585a85894bef95581a25fcb57c59ff0336
Sanjeev Premi <spremi@ymail.com>
2024-09-21 01:12:42 +0530

    feat: c: svc: state: Add zoom level
    Signed-off-by: Sanjeev Premi <spremi@ymail.com>

--- client/src/app/services/state.service.ts
+++ client/src/app/services/state.service.ts
@@ -21,6 +21,10 @@ export class StateService {
 
   private selected$: BehaviorSubject<Image> = new BehaviorSubject<Image>(initImage());
 
+  private zoom$: BehaviorSubject<number> = new BehaviorSubject<number>(1);
+
+  private zoom = 1;
+
   constructor() { }
 
   setLocation(location: string): void {
@@ -45,6 +49,30 @@ export class StateService {
   getSelected(): Observable<Image> {
     return this.selected$.asObservable().pipe(
       filter(c => c !== null),
+      distinctUntilChanged()
+    );
+  }
+
+  zoomIn(): void {
+    this.zoom += 0.5;
+
+    this.zoom$.next(this.zoom);
+  }
+
+  zoomOut(): void {
+    this.zoom = (this.zoom > 1) ? this.zoom - 0.5 : 1;
+
+    this.zoom$.next(this.zoom);
+  }
+
+  zoomNone(): void {
+    this.zoom = 1;
+
+    this.zoom$.next(this.zoom);
+  }
+
+  getZoom(): Observable<number> {
+    return this.zoom$.asObservable().pipe(
       distinctUntilChanged()
     );
   }
