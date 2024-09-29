d4a8b6c06b2840357c9d58427e0fdc3e7928b2e6
Sanjeev Premi <spremi@ymail.com>
2024-09-29 22:43:05 +0530

    feat: c: /home: Add a splash screen
    Signed-off-by: Sanjeev Premi <spremi@ymail.com>

--- client/src/app/routes/home/home.component.ts
+++ client/src/app/routes/home/home.component.ts
@@ -7,9 +7,10 @@
 //
 
 
+import { animate, style, transition, trigger } from '@angular/animations';
 import { Component, OnDestroy, OnInit } from '@angular/core';
 
-import { filter, first, Subscription, switchMap } from 'rxjs';
+import { filter, first, Subscription, switchMap, timer } from 'rxjs';
 
 import { Album } from '@models/album';
 import { Image } from '@models/image';
@@ -18,6 +19,7 @@ import { ViewRequest } from '@models/view-request';
 import { AlbumsComponent } from '@parts/albums/albums.component';
 import { ImagesComponent } from '@parts/images/images.component';
 import { PebblesComponent } from '@parts/pebbles/pebbles.component';
+import { SplashComponent } from '@parts/splash/splash.component';
 
 import { DataService } from '@services/data.service';
 import { StateService } from '@services/state.service';
@@ -29,6 +31,28 @@ import { StateService } from '@services/state.service';
     AlbumsComponent,
     ImagesComponent,
     PebblesComponent,
+    SplashComponent,
+  ],
+  animations: [
+    trigger('homeIn', [
+      transition(':enter', [
+        animate('2s', style({
+          opacity: 1
+        }))
+      ]),
+      transition(':leave', [
+        animate('500ms ease-out', style({
+          transform: 'translateX(100%)'
+        }))
+      ])
+    ]),
+    trigger('splashOut', [
+      transition(':leave', [
+        animate('250ms', style({
+          opacity: 0
+        }))
+      ])
+    ])
   ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.sass'
@@ -38,7 +62,11 @@ export class HomeComponent implements OnInit, OnDestroy {
   albums: Album[] = [];
   images: Image[] = [];
 
+  splash = false;
+
   private subs: Subscription[] = [];
+
+  private readonly SPLASH_TIME = 15000;
 
   constructor(
     private dataSvc: DataService,
@@ -46,6 +74,15 @@ export class HomeComponent implements OnInit, OnDestroy {
   ) {}
 
   ngOnInit(): void {
+    this.splash = this.stateSvc.getSplash();
+
+    if (!this.splash) {
+      timer(this.SPLASH_TIME).pipe(first()).subscribe(() => {
+        this.stateSvc.setSplash();
+        this.splash = true;
+      });
+    }
+
     const subLocation = this.stateSvc.getLocation().pipe(
       switchMap(o => {
         const request: ViewRequest = {
